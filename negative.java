1c59542d3fd65490e97bc93f3df696fc683021d8
megala1 <34705551+megala1@users.noreply.github.com>
2018-02-19 07:46:57 +0000

    Create negative.java

---
+++ negative.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class negative
+{
+    public static void main(String args[])
+    {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       if(n<0)
+       {
+           System.out.print("negative");
+       }
+       else
+       {
+           System.out.print("positive");
+       }
+    }
+}
